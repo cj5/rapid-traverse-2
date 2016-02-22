7ac91dc69b77e8dd782e6e6717f381938ff85d44
cj5 <stenny.cj@hotmail.com>
2016-02-21 21:45:49 -0600

    music section - mq's

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,31 @@
-// $(document).ready(function () {
-//     updateContainer();
-    
-//     $(window).resize(function() {
-//         updateContainer();
-//     });
-    
-
-//     function updateContainer() {
-//         var theHeight = $(window).height();
-//         $('.bg').css('height', theHeight);
-//     }
-
 $(document).ready(function(){
     $('#navicon').click(function(){
         $(this).toggleClass('open'); //(navicon transition)
         $('.menu').toggleClass('open'); //(adds nav menu)
     });
+
+    $('a').click(function(){
+        $('#navicon').toggleClass('open');
+        $('.menu').removeClass('open');
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
+
+    updateContainer();
+    
+    $(window).resize(function() {
+        updateContainer();
+    });
+    
+    function updateContainer() {
+        var theHeight = $(window).height();
+        $('.bg').css('height', theHeight);
+    }
 });
+
+
+    
 
 
